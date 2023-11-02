85774b9e87889fa9b9b16b4f243e60d06e272f5e
bismay <bismaypurkayastha5467@gmail.com>
2023-11-03 03:23:40 +0530

    User entity declared

---
+++ server/entities/user.entity.ts
@@ -0,0 +1,47 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { IsDateString, IsString, Validate } from "class-validator";
+
+export enum Gender {
+  Male='male',
+  Female='female',
+  Other='other',
+}
+
+export enum IsVaccinated {
+  Yes="yes",
+  No="no",
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  @IsString()
+  name: string;
+
+  @Column({ type: "enum", enum: Gender })
+  gender: Gender;
+
+  @Column({ name: "birth_date", type: "date" })
+  @IsDateString()
+  @Validate((birthdate: string) => {
+    const minDate = new Date("1903-01-01");
+    const maxDate = new Date();
+
+    if (new Date(birthdate) < minDate) {
+      return "Birthdate should be after 1903-01-01";
+    }
+
+    if (new Date(birthdate) > maxDate) {
+      return "Birthdate cannot be in the future";
+    }
+
+    return true;
+  })
+  birthDate: string;
+
+  @Column({ name: "is_vaccinated", type: "enum", enum: IsVaccinated })
+  isVaccinated: IsVaccinated;
+}
