8a409ce01bd745d0efe6c70db5d640631f7f7536
bismay <bismaypurkayastha5467@gmail.com>
2023-11-03 16:59:21 +0530

    proxy server configuration made.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/vote": "http://localhost:5000",
+      "/data": "http://localhost:5000",
+      "/counts": "http://localhost:5000",
+      "/results": "http://localhost:5000",
+    },
+  },
   plugins: [react()],
-})
+});
