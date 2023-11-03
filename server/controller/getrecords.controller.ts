e9c241cd5e3904e1c7ec9f677eee8fa2beda5fef
bismay <bismaypurkayastha5467@gmail.com>
2023-11-03 17:11:24 +0530

    file formatted

--- server/controller/getrecords.controller.ts
+++ server/controller/getrecords.controller.ts
@@ -4,34 +4,30 @@ import AppDataSource from "../config/database.config";
 import { SUCCESS_CODES } from "../../common/statusCode";
 import { User } from "../entities/user.entity";
 import { asyncHandler } from "../utils/asyncHandler";
-import { NO_OF_ROWS_PER_PAGE } from "./../../common/constants";
 
-const GetRecords = asyncHandler(
-  async (req: Request, res: Response) => {
+const NO_OF_ROWS_PER_PAGE = 6;
 
-    const page = parseInt(req.query.page as string) || 1;
-    const noOfRowsPerPage = NO_OF_ROWS_PER_PAGE;
+const GetRecords = asyncHandler(async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string) || 1;
+  const rowPerPage = parseInt(req.query.rowsPerPage as string) || 6;
 
-    const userRepository = AppDataSource.getRepository(User);
+  const userRepository = AppDataSource.getRepository(User);
 
-    const users = await userRepository
-      .createQueryBuilder("user")
-      .skip((page - 1) * noOfRowsPerPage)
-      .take(noOfRowsPerPage)
-      .getMany();
+  const users = await userRepository
+    .createQueryBuilder("user")
+    .skip((page - 1) * rowPerPage)
+    .take(rowPerPage)
+    .getMany();
 
-    const totalUsers = await userRepository
-      .createQueryBuilder("user")
-      .getCount();
+  const totalUsers = await userRepository.createQueryBuilder("user").getCount();
 
-    const hasMore = totalUsers > page * noOfRowsPerPage;
+  const hasMore = totalUsers > page * rowPerPage;
 
-    res.status(SUCCESS_CODES.OK).json({
-      users,
-      success: true,
-      hasMore
-    });
-  }
-);
+  res.status(SUCCESS_CODES.OK).json({
+    users,
+    success: true,
+    totalUsers,
+  });
+});
 
 export default GetRecords;
