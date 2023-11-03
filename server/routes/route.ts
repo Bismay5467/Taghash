5fe77d3d8e1a7c3b6f97f82115f27da3257580be
bismay <bismaypurkayastha5467@gmail.com>
2023-11-03 17:09:09 +0530

    file formatted using prettier

--- server/routes/route.ts
+++ server/routes/route.ts
@@ -1,20 +1,20 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from "express";
 
-import PostVote from '../controller/postvote.controller';
-import GetRecords from '../controller/getrecords.controller';
-import GetDataForLineChart from '../controller/getdataforlineChart.controller';
-import GetDataForBarChart from '../controller/getdataforbarChart.controller';
+import PostVote from "../controller/postvote.controller";
+import GetRecords from "../controller/getrecords.controller";
+import GetDataForLineChart from "../controller/getdataforlineChart.controller";
+import GetDataForBarChart from "../controller/getdataforbarChart.controller";
 
-import { validatePostVote } from '../middleware/postvote.middleware';
-import { validateGetRecord } from './../middleware/getrecords.middleware';
-import { validateDataForLineChart } from '../middleware/linechart.middleware';
+import { validatePostVote } from "../middleware/postvote.middleware";
+import { validateGetRecord } from "./../middleware/getrecords.middleware";
+import { validateDataForLineChart } from "../middleware/linechart.middleware";
 
 const router = express.Router();
 
-router.post('/vote', validatePostVote, PostVote);
+router.post("/vote", validatePostVote, PostVote);
 
-router.get('/data', validateGetRecord, GetRecords);
-router.get('/counts', validateDataForLineChart, GetDataForLineChart);
-router.get('/results', GetDataForBarChart);
+router.get("/data", validateGetRecord, GetRecords);
+router.get("/counts", validateDataForLineChart, GetDataForLineChart);
+router.get("/results", GetDataForBarChart);
 
 export default router;
